bb2889379af11bb5155879aa2d7e26e445b30d25
Morten Chami Berner <mob001@edu.zealand.dk>
2024-08-15 08:53:11 +0200

    Moved Razor page to another folder

--- Pages/CountryView.cshtml.cs
+++ Pages/CountryView.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Reflection;
 
-namespace GeograficApp.Pages.CatalogCountries
+namespace GeograficApp.Pages
 {
     public class CountryViewModel : PageModel
     {
